f878b006b08129f1beca5007472f8e6f47a6837f
Akshit Sisodiya <aakshit905@gmail.com>
2026-03-27 15:27:53 +0530

    deploy fix

--- chat-app-frontend/src/Lib/api.js
+++ chat-app-frontend/src/Lib/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v0",
-  // baseURL: "https://chat-app-6h3y.onrender.com/api/v0",
+  // baseURL: "http://localhost:8000/api/v0",
+  baseURL: "https://chat-app-6h3y.onrender.com/api/v0",
   withCredentials: true,
 });
 
--- chat-app-frontend/src/Lib/socket.js
+++ chat-app-frontend/src/Lib/socket.js
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
-  // const socket = io("https://chat-app-6h3y.onrender.com", {
-  transports: ["websocket"],
-  withCredentials: true,
-});
+const socket = io(
+  "https://chat-app-6h3y.onrender.com",
+  // "http://localhost:8000",
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  },
+);
 
 export default socket;
